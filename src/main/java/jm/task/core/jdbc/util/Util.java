5474f22af3a1836013548279f2c3df5d0da3e65a
herbal-egress <herbal.egress@gmail.com>
2025-04-29 12:20:14 +0500

    В Util использую поля класса в URL, USERNAME, PASSWORD в настройках SessionFactory вместо дублирования значений.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -40,9 +40,9 @@ public class Util {
                 Configuration configuration = new Configuration();
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/kata_pp_db");
-                settings.put(Environment.USER, "divan");
-                settings.put(Environment.PASS, "divan");
+                settings.put(Environment.URL, DB_URL);
+                settings.put(Environment.USER, DB_USERNAME);
+                settings.put(Environment.PASS, DB_PASSWORD);
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
